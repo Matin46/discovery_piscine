574edf00fcc859f538e4925ecc792cd800256c4e
Matin46 <suriyontum2@gmail.com>
2025-09-16 01:33:46 +0700

    4.2

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,10 @@
+num1 = float(input("Give me the first number: "))
+
+num2 = float(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{int(num1)} + {int(num2)} = {int(num1 + num2)}")
+print(f"{int(num1)} - {int(num2)} = {int(num1 - num2)}")
+print(f"{int(num1)} / {int(num2)} = {int(num1 / num2)}")
+print(f"{int(num1)} * {int(num2)} = {int(num1 * num2)}")
